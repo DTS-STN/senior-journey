6cb35d6c1ad7c5edcfd993cb1d6a14178990dd9a
Greg Baker <gregory.j.baker@hrsdc-rhdcc.gc.ca>
2023-03-03 17:18:54 -0330

    Clean up middleware.test.ts

--- frontend/__tests__/middleware.test.ts
+++ frontend/__tests__/middleware.test.ts
@@ -1,89 +1,72 @@
+import fetchMock from 'jest-fetch-mock'
 import { NextRequest, NextResponse } from 'next/server'
 
 import { middleware } from '../src/middleware'
 
-jest.mock('next/server', () => {
-  return {
-    NextRequest: jest.fn().mockImplementation((url: URL) => {
-      return {
-        nextUrl: { pathname: url.pathname, locale: 'default' },
-        url: url,
-      }
-    }),
-    NextResponse: jest.fn().mockImplementation((init: BodyInit | null) => {
-      return { init }
-    }),
-  }
-})
-
 describe('middleware', () => {
+  const nextConfig = {
+    i18n: { defaultLocale: 'default', locales: ['default', 'en', 'fr'] }
+  }
+
   const mockedStaticNext = jest.fn()
   const mockedStaticRedirect = jest.fn()
 
+  NextResponse.next = mockedStaticNext
+  NextResponse.redirect = mockedStaticRedirect
+
   beforeEach(() => {
+    fetchMock.doMock()
+    fetchMock.resetMocks()
+
     mockedStaticNext.mockClear()
     mockedStaticRedirect.mockClear()
-    // assign the mock jest.fn() to static method
-    NextResponse.next = mockedStaticNext
-    NextResponse.redirect = mockedStaticRedirect
-  })
-  ;['/_next/test', '/api/test', '/assets/favicon.ico'].forEach((pathname) => {
-    it('correctly detects non-page requests', () => {
-      const url = new URL(`https://example.com${pathname}`)
-      const request = new NextRequest(url)
-
-      middleware(request)
-
-      expect(mockedStaticNext).toBeCalled()
-      expect(mockedStaticRedirect).not.toBeCalled()
-    })
   })
 
-  it('correctly detects root route requests', () => {
-    const url = new URL('https://example.com/')
-    const request = new NextRequest(url)
+  it.each(['/_next/_buildManifest.js', '/api/livez', '/assets/favicon.ico'])('correctly detects non-page requests: %s', (pathname) => {
+    const request = new NextRequest(new URL(`https://example.com${pathname}`), { nextConfig })
 
     middleware(request)
 
     expect(mockedStaticNext).toBeCalled()
     expect(mockedStaticRedirect).not.toBeCalled()
   })
-  ;['en', 'fr'].forEach((locale) => {
-    it(`correctly detects root locale route requests - locale: ${locale}`, () => {
-      const url = new URL('https://example.com/')
-      const request = new NextRequest(url)
-      request.nextUrl.locale = locale
 
-      middleware(request)
+  it('correctly detects root route requests: /', () => {
+    const request = new NextRequest(new URL('https://example.com/'), { nextConfig })
 
-      expect(mockedStaticNext).not.toBeCalled()
-      expect(mockedStaticRedirect).toBeCalledWith(
-        new URL(`/${locale}/home`, 'https://example.com/')
-      )
-    })
+    middleware(request)
+
+    expect(mockedStaticNext).toBeCalled()
+    expect(mockedStaticRedirect).not.toBeCalled()
   })
 
-  it('correctly detects non-root router default locale requests - locale: default', () => {
-    const url = new URL('https://example.com/test')
-    const request = new NextRequest(url)
+  it.each(['en', 'fr'])('correctly detects root locale route requests: / → /%s/home', (locale) => {
+    const request = new NextRequest(new URL('https://example.com/'), { nextConfig })
+    request.nextUrl.locale = locale
 
     middleware(request)
 
     expect(mockedStaticNext).not.toBeCalled()
-    expect(mockedStaticRedirect).toBeCalledWith(
-      new URL(`/en/test`, 'https://example.com/')
-    )
+    expect(mockedStaticRedirect).toBeCalledWith(new URL(`/${locale}/home`, 'https://example.com/'))
   })
-  ;['en', 'fr'].forEach((locale) => {
-    it(`correctly detects non-root locale requests - locale: ${locale}`, () => {
-      const url = new URL(`https://example.com/${locale}/test`)
-      const request = new NextRequest(url)
-      request.nextUrl.locale = locale
 
-      middleware(request)
+  it('correctly detects non-root locale requests: /* → /en/*', () => {
+    const request = new NextRequest(new URL('https://example.com/test'), { nextConfig })
+    request.nextUrl.locale = 'default'
 
-      expect(mockedStaticNext).toBeCalled()
-      expect(mockedStaticRedirect).not.toBeCalled()
-    })
+    middleware(request)
+
+    expect(mockedStaticNext).not.toBeCalled()
+    expect(mockedStaticRedirect).toBeCalledWith(new URL(`/en/test`, 'https://example.com/'))
+  })
+
+  it.each(['en', 'fr'])('correctly detects non-root locale requests: /%s/*', (locale) => {
+    const request = new NextRequest(new URL(`https://example.com/${locale}/test`), { nextConfig })
+    request.nextUrl.locale = locale
+
+    middleware(request)
+
+    expect(mockedStaticNext).toBeCalled()
+    expect(mockedStaticRedirect).not.toBeCalled()
   })
 })
