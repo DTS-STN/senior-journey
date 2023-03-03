1c209f5885a11f1cabb60e93303a70f761a54801
Greg Baker <gregory.j.baker@hrsdc-rhdcc.gc.ca>
2023-03-03 07:46:17 -0330

    Add kubernetes liveness/readiness endpoints

---
+++ frontend/src/pages/api/readyz.ts
@@ -0,0 +1,11 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+/**
+ * An API endpoint to be used in kubernetes readyness checks.
+ */
+export default function handler(_req: NextApiRequest, res: NextApiResponse) {
+  res.status(200).json({
+    status: 'UP',
+    uptime: `${process.uptime()}s`
+  })
+}
