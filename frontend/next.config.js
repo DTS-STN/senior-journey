2d0250b30ff41fad785b055be28592f612cdd1b8
Greg Baker <gregory.j.baker@hrsdc-rhdcc.gc.ca>
2023-03-17 08:30:46 -0230

    [frontend] Add OpenTelemetry metrics and tracing

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,9 @@
 // @ts-check
+
+require('./open-telemetry.config')
+
 const { i18n } = require('./next-i18next.config')
-const fs = require('fs')
+const { statSync } = require('fs')
 
 // prettier-ignore
 const securityHeaders = [
@@ -18,10 +21,10 @@ const securityHeaders = [
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    NEXT_PUBLIC_BUILD_DATE: new Date(process.env.BUILD_DATE ?? fs.statSync('package.json').mtime).toISOString().slice(0, 10),
+    NEXT_PUBLIC_BUILD_DATE: new Date(process.env.BUILD_DATE ?? statSync('package.json').mtime).toISOString().slice(0, 10),
     NEXT_PUBLIC_BUILD_ID: process.env.BUILD_ID ?? '0000',
     NEXT_PUBLIC_BUILD_REVISION: process.env.BUILD_REVISION ?? '00000000',
-    NEXT_PUBLIC_BUILD_TIMESTAMP: new Date(process.env.BUILD_DATE ?? fs.statSync('package.json').mtime).toISOString(),
+    NEXT_PUBLIC_BUILD_TIMESTAMP: new Date(process.env.BUILD_DATE ?? statSync('package.json').mtime).toISOString(),
     NEXT_PUBLIC_BUILD_VERSION: process.env.BUILD_VERSION ?? '00000000-0000-00000000',
     LOGGING_LEVEL: process.env.LOGGING_LEVEL ?? 'info',
   },
