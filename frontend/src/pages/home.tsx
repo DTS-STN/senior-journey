16979d006bf0f8f3538a601f26fb9052d858b07f
CurtisUnderwood <curtisund@gmail.com>
2023-03-20 09:09:37 -0400

    fix: missing text

--- frontend/src/pages/home.tsx
+++ frontend/src/pages/home.tsx
@@ -99,7 +99,7 @@ const Home: FC = () => {
           <h3 className="text-xl font-bold border-b pb-2">{t('contact-us.cards.call-us.title')}</h3>
           <p className="pt-3 text-lg">{t('contact-us.cards.call-us.description')}</p>
           <p className="text-lg md:inline"><span className="font-bold">{t('contact-us.cards.call-us.toll-free')}</span> {t('contact-us.cards.call-us.toll-number')}</p>
-          <p className="text-lg md:inline md:ml-10"><span className="font-bold">{t('direct')}</span> {t('contact-us.cards.call-us.direct-number')}</p>
+          <p className="text-lg md:inline md:ml-10"><span className="font-bold">{t('contact-us.cards.call-us.direct')}</span> {t('contact-us.cards.call-us.direct-number')}</p>
         </div>
         <div className="text-basic-gray card border border-gray-300 text-gray-900 shadow-sm rounded-md p-4">
           <h3 className="text-xl font-bold border-b pb-2">{t('contact-us.cards.find-office.title')}</h3>
