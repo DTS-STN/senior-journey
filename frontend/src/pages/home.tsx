21ac3e074685b79a40b4dbebb9b1d1c55b091e6e
Greg Baker <gregory.j.baker@hrsdc-rhdcc.gc.ca>
2023-03-24 13:37:01 -0230

    [frontend] Code cleanup and polish

--- frontend/src/pages/home.tsx
+++ frontend/src/pages/home.tsx
@@ -7,7 +7,6 @@ import { NextSeo } from 'next-seo'
 import Link from 'next/link'
 import Image from 'next/image'
 
-
 export interface SupportingSeniorsCardProps {
   src: string,
   href: string,
@@ -16,21 +15,20 @@ export interface SupportingSeniorsCardProps {
 }
 
 const SupportingSeniorsCard: FC<SupportingSeniorsCardProps> = ({src, href, linkText, text}) => {
-return (
-  <div className="flex flex-col border rounded-md shadow-lg p-4">
-
-      <div className="h-[300px] mx-auto">
+  return (
+    <div className="flex flex-col border rounded-md shadow-lg p-4">
+        <div className="h-[300px] mx-auto">
           <Image
-              src={src}
-              width={200}
-              height={300}
-              alt=""
+            src={src}
+            width={200}
+            height={300}
+            alt=""
           />
-      </div>
-      <a href={href} target='_blank' rel="noopener noreferrer">{linkText}</a>
-      <p>{text}</p>
-  </div>
-)
+        </div>
+        <a href={href} target='_blank' rel="noopener noreferrer">{linkText}</a>
+        <p>{text}</p>
+    </div>
+  )
 }
 
 const Home: FC = () => {
@@ -92,15 +90,15 @@ const Home: FC = () => {
 
       <section>
         <h2 className="h2">{t('contact-us.title')}</h2>
-        <p className="text-black pb-4">{t('contact-us.description')}</p>
-        <div className="text-basic-gray card border border-gray-300 text-gray-900 shadow-sm rounded-md p-4 mb-6">
+        <p className="pb-4">{t('contact-us.description')}</p>
+        <div className="card border border-gray-300 shadow-sm rounded-md p-4 mb-6">
           <h3 className="text-xl font-bold border-b pb-2">{t('contact-us.cards.call-us.title')}</h3>
           <p className="pt-3 text-lg">{t('contact-us.cards.call-us.description')}</p>
           <p className="text-lg md:inline"><span className="font-bold">{t('contact-us.cards.call-us.toll-free')}</span> {t('contact-us.cards.call-us.toll-number')}</p>
           <p className="text-lg md:inline md:ml-10"><span className="font-bold">{t('contact-us.cards.call-us.direct')}</span> {t('contact-us.cards.call-us.direct-number')}</p>
         </div>
-        <div className="text-basic-gray card border border-gray-300 text-gray-900 shadow-sm rounded-md p-4">
-          <h3 className="text-xl font-bold border-b pb-2">{t('contact-us.cards.find-office.title')}</h3>
+        <div className="card border border-gray-300 shadow-sm rounded-md p-4">
+          <h3 className="text-xl font-bold border-b pb-2">{t('contact-us.cards.find-office.title')} adfasdfad</h3>
           <p className="pt-3 text-lg">{t('contact-us.cards.find-office.description')}</p>
           <Link className="text-lg" href={t('contact-us.cards.find-office.href')}>{t('contact-us.cards.find-office.link-text')}</Link>
         </div>
@@ -110,9 +108,7 @@ const Home: FC = () => {
 }
 
 export const getServerSideProps: GetServerSideProps = async ({ locale }) => ({
-  props: {
-    ...(await serverSideTranslations(locale ?? 'default', ['common', 'home'])),
-  },
+  props: { ...(await serverSideTranslations(locale ?? 'default', ['common', 'home'])) }
 })
 
 export default Home
