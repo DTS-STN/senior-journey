a76b9420acade73b61ed99975a9a8e32ce3ce484
Greg Baker <gregory.j.baker@hrsdc-rhdcc.gc.ca>
2023-04-06 12:46:30 -0230

    Fix broken middleware processing

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -2,15 +2,49 @@ import { NextRequest, NextResponse } from 'next/server'
 
 import { getLogger } from './logging/log-util'
 
+/**
+ * Checks whether a given pathname is excluded based on a list of excluded paths.
+ *
+ * @param {string} pathname - The pathname to check.
+ * @returns {boolean} - true if the pathname is excluded, false otherwise.
+ */
+function isExcluded(pathname: string) {
+  const excludedPaths = ['/_next/', '/api/', '/assets/']
+  return excludedPaths.some((excludedPath) => pathname.includes(excludedPath))
+}
+
+/**
+ * Middleware function that processes incoming requests and returns the appropriate response.
+ *
+ * @param {NextRequest} req - The incoming request object.
+ * @returns {NextResponse} - The appropriate response for the incoming request.
+ */
 export function middleware(req: NextRequest) {
   const { nextUrl, url } = req
   const { locale, pathname } = nextUrl
   const logger = getLogger('middleware')
 
-  if (pathname === '/' && locale !== 'default') {
-    logger.debug(`Request for /${locale}/ will be redirected to /${locale}/home`)
+  logger.trace(`Incoming request for [${pathname}]`)
+
+  if (isExcluded(pathname)) {
+    logger.trace(`Request for excluded path [${pathname}] is not a candidate for processing`)
+    return NextResponse.next()
+  }
+
+  logger.trace(`Processing request for [${pathname}]`)
+
+  if (locale === 'default' && pathname !== '/') {
+    logger.trace(`Requested path [${pathname}] does not include required locale of [en|fr]`)
+
+    req.nextUrl.pathname = '/404'
+    return NextResponse.rewrite(req.nextUrl, { status: 404 })
+  }
+
+  if ((locale === 'en' || locale === 'fr') && pathname === '/') {
+    logger.debug(`Requested path /${locale}/ will be redirected to /${locale}/home`)
     return NextResponse.redirect(new URL(`/${locale}/home`, url))
   }
 
+  logger.trace(`Processing completed for request [${pathname}]`)
   return NextResponse.next()
 }
