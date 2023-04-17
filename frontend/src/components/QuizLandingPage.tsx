0818d39ec9abdb185db9101c9fef096b1cda3a41
CurtisUnderwood <curtisund@gmail.com>
2023-04-17 12:48:20 -0400

    fix: lint issue

--- frontend/src/components/QuizLandingPage.tsx
+++ frontend/src/components/QuizLandingPage.tsx
@@ -8,10 +8,10 @@ const QuizLandingPage = () => {
               Retirement Ready Quiz
           </h2>
         </section>
-        <p>Not sure you&#39;ve checked all the boxes to retire? Take this quiz as a first step towards your retirement checklist.</p>
+        <p>Not sure you&apos;ve checked all the boxes to retire? Take this quiz as a first step towards your retirement checklist.</p>
         <p>Your answers will help identify government pensions and benefits you could receive. They will also guide you on what to consider to better prepare for retirement.</p>
-        <h6 className="font-bold font-display text-xl mb-4">What you&#39;ll need</h6>
-        <p>You don&#39;t need any special information to take this quiz. To create the most appropriate retirement checklist, you will need to provide information on the following:</p>
+        <h6 className="font-bold font-display text-xl mb-4">What you&apos;ll need</h6>
+        <p>You don&apos;t need any special information to take this quiz. To create the most appropriate retirement checklist, you will need to provide information on the following:</p>
         <ul className="list-disc ml-6">
             <li><span className="font-bold">legal status</span> (such as Canadian citizen, Indian Status, or permanent resident)</li>
             <li><span className="font-bold">residence history</span> (such as Canadian citizen, Indian Status, or permanent resident)</li>
