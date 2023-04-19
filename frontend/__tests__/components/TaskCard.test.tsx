d9f521c4d9dd9cdc2813bb8236a2b2d80d8e931c
Sébastien Comeau <sebastien.comeau@hrsdc-rhdcc.gc.ca>
2023-04-19 15:50:09 -0300

    remove react-icons package

--- frontend/__tests__/components/TaskCard.test.tsx
+++ frontend/__tests__/components/TaskCard.test.tsx
@@ -1,7 +1,9 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect';
-import TaskCard, { Task } from '../../src/components/TaskCard';
+import React from 'react'
+
+import '@testing-library/jest-dom/extend-expect'
+import { render } from '@testing-library/react'
+
+import TaskCard, { Task } from '../../src/components/TaskCard'
 
 describe('TaskCard', () => {
   const task: Task = {
@@ -10,17 +12,15 @@ describe('TaskCard', () => {
     linkTitle: 'Test Link',
     links: 'https://example.com',
     tags: 'tag1 tag2',
-  };
+  }
 
   it('renders the TaskCard component', () => {
-    const { container } = render(<TaskCard task={task} />);
+    const { container } = render(<TaskCard task={task} />)
 
-    expect(container.textContent).toContain('Test Task');
-    expect(container.textContent).toContain('Test Link'); 
-    expect(container.textContent).toContain('https://example.com');
-    expect(container.textContent).toContain('tag1');
-    expect(container.textContent).toContain('tag2');
-    
-
-  });
-});
+    expect(container.textContent).toContain('Test Task')
+    expect(container.textContent).toContain('Test Link')
+    expect(container.textContent).toContain('https://example.com')
+    expect(container.textContent).toContain('tag1')
+    expect(container.textContent).toContain('tag2')
+  })
+})
