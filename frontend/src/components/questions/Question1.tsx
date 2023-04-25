6bee3cc5c7cabc67212f267a84f5bd5db68cfc70
frank basham <frankabasham@gmail.com>
2023-04-25 09:13:08 -0400

    quiz questions

--- frontend/src/components/questions/Question1.tsx
+++ frontend/src/components/questions/Question1.tsx
@@ -1,10 +1,90 @@
+import React from 'react'
+
+import { ToggleButton, ToggleButtonGroup } from '@mui/material'
+import { useTranslation } from 'react-i18next'
+
 const Question1 = () => {
-    return (
-     <div>
-        <h5>When do you plan on retiring?</h5>
-     </div>
-    );
-  };
-  
-  export default Question1;
-  
+  let { t } = useTranslation('learn')
+  const [alignment, setAlignment] = React.useState('web')
+
+  const handleAlignment = (
+    event: React.MouseEvent<HTMLElement>,
+    newAlignment: string
+  ) => {
+    setAlignment(newAlignment)
+  }
+
+  return (
+    <div>
+      <h5 className="font-display text-2xl font-light">
+        {t('quiz.questions.question-1.title')}
+      </h5>
+      <ToggleButtonGroup
+        orientation="vertical"
+        exclusive
+        fullWidth={true}
+        className="my-4"
+        value={alignment}
+        onChange={handleAlignment}
+        sx={{
+          '& .MuiToggleButton-root:not(:first-of-type)': {
+            borderTop: '1px solid #e1e4e7',
+            borderRadius: '4px',
+          },
+          '.MuiToggleButton-root.Mui-selected': {
+            'backgroundColor': '#004f56',
+            'color': '#f1f1f1',
+            '&.Mui-selected:hover': {
+              backgroundColor: '#004f56',
+            },
+          },
+        }}
+      >
+        <ToggleButton
+          value="before-60"
+          aria-label={t('quiz.questions.question-1.option-1')}
+          className="my-4 font-display text-base font-bold normal-case"
+        >
+          {t('quiz.questions.question-1.option-1')}
+        </ToggleButton>
+        <ToggleButton
+          value="between-60-65"
+          aria-label={t('quiz.questions.question-1.option-2')}
+          className="my-4 font-display text-base font-bold normal-case"
+        >
+          {t('quiz.questions.question-1.option-2')}
+        </ToggleButton>
+        <ToggleButton
+          value="at-65"
+          aria-label={t('quiz.questions.question-1.option-3')}
+          className="my-4 font-display text-base font-bold normal-case"
+        >
+          {t('quiz.questions.question-1.option-3')}
+        </ToggleButton>
+        <ToggleButton
+          value="between-65-70"
+          aria-label={t('quiz.questions.question-1.option-4')}
+          className="my-4 font-display text-base font-bold normal-case"
+        >
+          {t('quiz.questions.question-1.option-4')}
+        </ToggleButton>
+        <ToggleButton
+          value="afer-70"
+          aria-label={t('quiz.questions.question-1.option-5')}
+          className="my-4 font-display text-base font-bold normal-case"
+        >
+          {t('quiz.questions.question-1.option-5')}
+        </ToggleButton>
+        <ToggleButton
+          value="not-sure"
+          aria-label={t('quiz.questions.question-1.option-6')}
+          className="my-4 font-display text-base font-bold normal-case"
+        >
+          {t('quiz.questions.question-1.option-6')}
+        </ToggleButton>
+      </ToggleButtonGroup>
+    </div>
+  )
+}
+
+export default Question1
