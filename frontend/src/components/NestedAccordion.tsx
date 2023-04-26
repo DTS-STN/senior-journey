10b2670d3e6f98b33309661581a7f73b8dc5dc9e
ZhuZhuThanou <blogicalbmath@gmail.com>
2023-04-26 10:14:14 -0400

    Used MUI Accordion to handle keyboard navigation

--- frontend/src/components/NestedAccordion.tsx
+++ frontend/src/components/NestedAccordion.tsx
@@ -1,83 +1,57 @@
-import React, { useState } from 'react';
-import ExpandLessIcon from '@mui/icons-material/ExpandLess';
+import React from 'react';
+import TaskCard, { Task } from './TaskCard';
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
-import TaskCard, { Task } from './TaskCard';
-
-interface AccordionProps {
-    sectionTitle: string,
-    subSectionTitle: string,
-    children: React.ReactNode
+interface NestedAccordionProps {
+    sectionTitle: string;
+    subSectionTitle: string;
+    tasks: Task[];
 }
 
-const Accordion: React.FC<AccordionProps> = ({ sectionTitle = "", subSectionTitle = "", children }) => {
-    const [isOpen, setIsOpen] = useState(false);
-
+const NestedAccordion: React.FC<NestedAccordionProps> = ({
+    sectionTitle,
+    subSectionTitle,
+    tasks = [],
+}) => {
     return (
         <div className="mb-4 container mx-auto">
-            <div
-                className="bg-[#00363C] text-white  px-4 pt-6 py-2 cursor-pointer flex flex-col justify-between"
-                onClick={() => setIsOpen(!isOpen)}
-            >
-                <div className="flex items-center">
-                    <span>{sectionTitle}</span>
-                    <div className="ml-auto">
-                        {isOpen ? <ExpandLessIcon /> : <ExpandMoreIcon />}
+            <Accordion>
+                <AccordionSummary
+                    className="bg-[#00363C] text-white"
+                    expandIcon={tasks.length > 0 ? <ExpandMoreIcon className="text-white" /> : null}
+                >
+                    <div className="flex flex-col justify-between w-full">
+                        <div className="flex items-center">
+                            <span>{sectionTitle}</span>
+                        </div>
+                        <div>
+                            <span className="text-xs text-gray-400">
+                                {subSectionTitle}
+                            </span>
+                        </div>
                     </div>
-                </div>
-                <div>
-                    <span className="text-xs text-gray-400">{subSectionTitle}</span>
-                </div>
-            </div>
-            {isOpen && (
-                <div className="border">
-                    {children}
-                </div>
-            )}
+                </AccordionSummary>
+                {tasks.map((task) => (
+                    <Accordion key={task.id}>
+                        <AccordionSummary className="bg-white"
+                            expandIcon={tasks.length > 0 ? <ExpandMoreIcon /> : null}
+                        >
+                            <div className="flex flex-col justify-between w-full">
+                                <div className="flex items-center">
+                                    <span>{task.title}</span>
+                                </div>
+                            </div>
+                        </AccordionSummary>
+                        <AccordionDetails>
+                            <TaskCard showCheckbox={false} task={task} />
+                        </AccordionDetails>
+                    </Accordion>
+                ))}
+            </Accordion>
         </div>
-    );
-};
-
-const AccordionInside: React.FC<Task> = (task) => {
-    const [isOpenInner, setIsOpenInner] = useState(false);
-    return (
-        <div>
-            <div
-                className="bg-white text-black px-4 pt-6 py-2 border cursor-pointer flex justify-between items-center"
-                onClick={() => setIsOpenInner(!isOpenInner)}
-            >
-                <span>{task.title}</span>
-                <strong>
-                    {isOpenInner ? <ExpandLessIcon /> : <ExpandMoreIcon />}
-                </strong>
-            </div>
-            {isOpenInner && (
-
-                <TaskCard showCheckbox={false}
-                    task={task} />
-            )}
-        </div>
-    );
-};
-
-
-
-interface NestedAccordionProps {
-    sectionTitle: string,
-    subSectionTitle: string,
-    tasks: Task[]
-}
-
-const NestedAccordion: React.FC<NestedAccordionProps> = ({ sectionTitle, subSectionTitle, tasks = [] }) => {
-    return (
-        <Accordion sectionTitle={sectionTitle} subSectionTitle={subSectionTitle}>
-            {tasks.map((task) => (
-                <div key={task.id}>
-                    <AccordionInside {...task} />
-                </div>
-            ))}
-
-        </Accordion>
     );
 };
 
