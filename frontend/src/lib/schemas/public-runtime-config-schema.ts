1b169afccf1b6d0a34329ba6ef8a590cd02519c3
Sébastien Comeau <sebastien.comeau@hrsdc-rhdcc.gc.ca>
2023-06-22 12:19:49 -0300

    [frontend] analytics beacon execution timeout

--- frontend/src/lib/schemas/public-runtime-config-schema.ts
+++ frontend/src/lib/schemas/public-runtime-config-schema.ts
@@ -4,6 +4,11 @@ export const publicRuntimeConfigSchema = yup.object({
   NEXT_PUBLIC_ADOBE_ANALYTICS_SCRIPT_SRC: yup
     .string()
     .url('Environment variable NEXT_PUBLIC_ADOBE_ANALYTICS_SCRIPT_SRC must be a valid URL'),
+  NEXT_PUBLIC_ANALYTICS_BEACON_DELAY: yup
+    .number()
+    .integer('Environment variable NEXT_PUBLIC_ANALYTICS_BEACON_DELAY must be an integer')
+    .min(0, 'Environment variable NEXT_PUBLIC_ANALYTICS_BEACON_DELAY must be greater than or equal to 0')
+    .default(() => 250),
   NEXT_PUBLIC_APP_BASE_URI: yup
     .string()
     .required('Environment variable NEXT_PUBLIC_APP_BASE_URI is required')
